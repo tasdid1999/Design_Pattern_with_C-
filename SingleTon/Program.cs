1867fae9ea3495cc1fbb1cb81cdebb0d1eb705e7
tasdid <tasdi@LAPTOP-D02SRM5Q>
2023-08-11 19:06:18 +0600

    signleton practice

---
+++ SingleTon/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace DesignPattern.SingleTon
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+
+            var obj = SingleTon.GetInstance();
+            var obj2 = SingleTon.GetInstance();
+
+
+        }
+    }
+    public sealed class SingleTon
+    {
+        private static SingleTon? singletonObject = null; 
+        private SingleTon(){}
+
+        public static SingleTon GetInstance()
+        { 
+        
+            if(singletonObject is null)
+            {
+                Console.WriteLine("hello");
+                singletonObject = new SingleTon();
+            }
+
+            return singletonObject;
+        }
+    }
+}
